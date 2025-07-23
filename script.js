d289d929c16303be5dd619b9fbfd082194230255
t0ryme <v.ulloapereira@gmail.com>
2025-07-23 01:14:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,33 @@ const ramos = [
   { id: "A", nombre: "Álgebra", semestre: "2º", requisitos: ["F.M"]},
   { id: "I.I.E", nombre: "Ingeniería, innovación y emprendimiento", semestre "2º", requisitos: ["D.I"]},
   { id: "P", nombre: "Programación", semestre "2º", requisitos: ["F.P"]},
+
+   /* SEGUNDO AÑO */
+   /* 3º semestre */
+   { id: "C.I.S", nombre: "Cálculo integral y series", semestre: "3º", requisitos: ["C.D"]},
+   { id: "A.L", nombre:
   
   
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Malla Curricular</title>
+
+  <!-- Fuentes desde Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Playwrite+AU+QLD:wght@100;400&family=Winky+Rough&display=swap" rel="stylesheet">
+
+  <!-- Estilos -->
+  <link rel="stylesheet" href="estilos.css" />
+</head>
+<body>
+  <h1>Malla Curricular</h1>
+
+  <!-- Contenedor de la malla -->
+  <div id="malla" class="grid"></div>
+
+  <!-- Script funcional -->
+  <script src="script.js"></script>
+</body>
+</html>
